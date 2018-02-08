e99fe44645f6d985bc2ea9089cb54a268a559de3
Florian Roth <florian.roth@bsk-consulting.de>
2018-02-08 08:33:39 +0100

    Removed build folder from repo

--- build/test-sets/execution/remote-exe-tools.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-ECHO ===========================================================================
-ECHO REMOTE EXECUTION TOOL 
-ECHO.
-ECHO Dropping a remote execution tool into the APT dir
-ping -n 5 127.0.0.1 > NUL
-
-%ZIP% e -p%PASS% %TOOLARCH% -aoa -o%APTDIR% toolset\xCmd.exe > NUL
