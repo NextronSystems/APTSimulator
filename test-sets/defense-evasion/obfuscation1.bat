d0a741a5a15ad19d19f097723d3800bd633b19e0
NaimishP <36006376+NaimishP@users.noreply.github.com>
2020-03-25 15:07:31 -0500

    Update obfuscation1.bat
    Single line command update

--- test-sets/defense-evasion/obfuscation1.bat
+++ test-sets/defense-evasion/obfuscation1.bat
@@ -3,7 +3,4 @@
 ECHO ===========================================================================
 ECHO OBFUSCATION
 ECHO Dropping obfuscated RAR file with JPG extension
-ping -n 5 127.0.0.1 > NUL
-
-"%ZIP%" e -p%PASS% "%FILEARCH%" -aoa -o"%TEMP%" workfiles\s01.jpg > NUL
-
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%FILEARCH%" -aoa -o"%TEMP%" workfiles\s01.jpg > NUL
