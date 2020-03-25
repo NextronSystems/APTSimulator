2068f6ec35968e2c92896b0b0aa7768b44e917a0
NaimishP <36006376+NaimishP@users.noreply.github.com>
2020-03-25 15:14:09 -0500

    Update web-shells.bat
    Fixed ping

--- test-sets/persistence/web-shells.bat
+++ test-sets/persistence/web-shells.bat
@@ -3,9 +3,7 @@
 ECHO ===========================================================================
 ECHO WEBSHELLS
 ECHO Dropping web shell in new WWW directory
-ping -n 5 127.0.0.1 > NUL
-
-MKDIR "%WWWROOT%"
-"%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\b.jsp > NUL
-"%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\tests.jsp > NUL
-"%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\shell.gif > NUL
+ping -n 5 127.0.0.1 > NUL MKDIR "%WWWROOT%"
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\b.jsp > NUL
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\tests.jsp > NUL
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\shell.gif > NUL
