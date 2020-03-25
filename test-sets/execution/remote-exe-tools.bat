a2378265dc9c9a9177e8b31a725fac07c21fca24
NaimishP <36006376+NaimishP@users.noreply.github.com>
2020-03-25 15:10:14 -0500

    Update remote-exe-tools.bat
    Single line command update

--- test-sets/execution/remote-exe-tools.bat
+++ test-sets/execution/remote-exe-tools.bat
@@ -3,6 +3,4 @@
 ECHO ===========================================================================
 ECHO REMOTE EXECUTION TOOL 
 ECHO Dropping a remote execution tool into the APT dir
-ping -n 5 127.0.0.1 > NUL
-
-"%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%APTDIR%" toolset\xCmd.exe > NUL
+ping -n 5 127.0.0.1 > NUL "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%APTDIR%" toolset\xCmd.exe > NUL
