21d652a14e11e94535c323537791feab4412b4f1
phantinuss <79651203+phantinuss@users.noreply.github.com>
2022-06-20 10:08:11 +0200

    feat: add IIS log of hafnium exploitation

--- test-sets/persistence/web-shells.bat
+++ test-sets/persistence/web-shells.bat
@@ -9,3 +9,4 @@ MKDIR "%WWWROOT%"
 "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\b.jsp > NUL
 "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\tests.jsp > NUL
 "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\shell.gif > NUL
+"%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"C:\inetpub\logs\LogFiles\W3SVC2" toolset\hafnium-dummy.log > NUL
