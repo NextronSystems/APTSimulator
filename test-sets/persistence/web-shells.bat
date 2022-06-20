d7236106e2b1ad00824671f97f9272abd3bec963
phantinuss <79651203+phantinuss@users.noreply.github.com>
2022-06-20 10:14:55 +0200

    feat: add bmp cloaked jsp webshell

--- test-sets/persistence/web-shells.bat
+++ test-sets/persistence/web-shells.bat
@@ -9,4 +9,5 @@ MKDIR "%WWWROOT%"
 "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\b.jsp > NUL
 "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\tests.jsp > NUL
 "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\shell.gif > NUL
+"%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"%WWWROOT%" toolset\image.bmp > NUL
 "%ZIP%" e -p%PASS% "%TOOLARCH%" -aoa -o"C:\inetpub\logs\LogFiles\W3SVC2" toolset\hafnium-dummy.log > NUL
